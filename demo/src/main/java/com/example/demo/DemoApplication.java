3ec72611dc25acd1c4dc060e8ba44442fb1252b3
ever1015 <ever1015@163.com>
2020-07-01 15:09:15 +0800

    Merge branch 'master' of https://github.com/ever1015/demo
    # Conflicts:
    #	demo/src/main/java/com/example/demo/controller/DemoController.java

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,16 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+//@EnableScheduling
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/hi")
-    public String  hi (){
-        return "hi  I'am ever";
-    }
 }
